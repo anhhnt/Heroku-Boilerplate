cd2fd744cc4aaaca217bc8e699fd392831b81561
Anh Hoang <ntahoang@gmail.com>
2016-05-06 22:54:12 +0700

    Added sending test message by visiting app via browser

--- index.js
+++ index.js
@@ -17,10 +17,42 @@ bot.startRTM(function(err,bot,payload) {
 bot.configureIncomingWebhook({
   url : process.env.SLACK_WEBHOOK_URL
 });
-bot.sendWebhook({
-  text: " A test webhook message",
-  channel : '#taa-testing'
-});
+var sendTestMsg = function (msg) {
+  bot.sendWebhook({
+    text: msg || " A test webhook message",
+    channel : '#taa-testing'
+  });
+};
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+var counter = 0;
+
+http.createServer(function (req, res) {
+
+  	// increment the counter for each visitor request
+  	counter=counter+1;
+
+	var path = req.url;
+	console.log("requested=" + path + " counter=" + counter);
+
+	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
+
+	if (path == "/") {
+		res.end("Hello World. You are requestor # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
+
+	} else if (path == "/page2") {
+		res.end("This is page 2. <a href='/'>Back.</a>\n"); // send response and close connection
+	} else {
+    sendTestMsg(path);
+    res.end('');
+  }
+}).listen(port);
+
+// console info message
+console.log('Server running at http://127.0.0.1:' + port);
+
 //
 // // Daily tasks:
 //
